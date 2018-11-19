15fc4304301a2db9bd82a21801ecefc8ead3e0dc
Draltan Marin <moi@draltan.com>
2018-11-19 14:27:43 +0100

    feat: start sign with metamask component

---
+++ demo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render} from 'react-dom';
+import Sign from '../src/Sign';
+
+const App = () => (
+  <Sign/>
+);
+
+render(<App />, document.getElementById('root'));
